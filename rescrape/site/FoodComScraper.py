33c992e0aae59d86557a030f9c206845441dab7f
Roy Gvirtsman <roy@gvirtsman.com>
2017-07-16 23:24:46 -0700

    Fixed food.com
    Use like with allRecipes:
    s =
    rescrape.site.FoodCom("http://www.food.com/recipe/oven-roasted-rib-eye-steak-455863?ic1=recentlyviewed")
    s.write()

--- rescrape/site/FoodComScraper.py
+++ rescrape/site/FoodComScraper.py
@@ -1,45 +1,59 @@
 #!/usr/bin/env python
 # encoding: utf-8
-
+import json
 from AbstractScraper import RecipeScraper
 
 
 class FoodCom(RecipeScraper):
 
     def title(self):
-        title = self.find("h1", {"itemprop": "name"})
-        return title.string.encode("ascii", 'ignore').strip()
+        # return self.soup.title.string.encode("ascii", 'ignore').strip().split(" - ")[0]
+        js = self.soup.find("div", {"class": "fd-page-feed"}).find("script", {"type": "application/ld+json"}).contents[0]
+        l = json.loads(js)
+        title = l["name"]
+        return title
 
     def url(self):
         return self.url
 
     def num_servings(self):
-        return self.find("input", {"id": "original_value"})["value"].encode("ascii", 'ignore')
+        return self.soup.find("li", {"id": "yield-servings"}).find("span", {"class": "count"}).contents[0]
 
     def prep_time(self):
-        duration = self.find("meta", {"itemprop": "prepTime"})["content"].encode("ascii", 'ignore')
+        js = self.soup.find("div", {"class": "fd-page-feed"}).find("script", {"type": "application/ld+json"}).contents[0]
+        l = json.loads(js)
+        duration = l["prepTime"]
         return self.parse_duration(duration)
 
     def cook_time(self):
-        duration = self.find("meta", {"itemprop": "cookTime"})["content"].encode("ascii", 'ignore')
+        js = self.soup.find("div", {"class": "fd-page-feed"}).find("script", {"type": "application/ld+json"}).contents[0]
+        l = json.loads(js)
+        duration = l["cookTime"]
         return self.parse_duration(duration)
 
     def total_time(self):
-        duration = self.find("meta", {"itemprop": "totalTime"})["content"].encode("ascii", 'ignore')
+        js = self.soup.find("div", {"class": "fd-page-feed"}).find("script", {"type": "application/ld+json"}).contents[0]
+        l = json.loads(js)
+        duration = l["totalTime"]
         return self.parse_duration(duration)
 
     def ingredients(self):
-        ings = self.findAll("span", {"class": 'ingredient'})
-        for ing in ings:
-            dirs = map(lambda x: x.encode("ascii", "ignore").strip(), ing.findAll(text=True))
-            dirs = [d for d in dirs if d != ""]
-            dirs = ' '.join(dirs).strip()
-            dirs = dirs.replace(" ,", ",")
-            yield ' '.join(dirs.split())
+        js = self.soup.find("div", {"class": "fd-page-feed"}).find("script", {"type": "application/ld+json"}).contents[0]
+        l = json.loads(js)
+        ingredients = l["recipeIngredient"]
+        for ing in ingredients:
+            yield ing
 
     def directions(self):
-        for s in self.find("span", {"class": "instructions"}).findAll("li"):
-            yield s.find("div", {"class", "txt"}).string.encode("ascii", "ignore")
+        js = self.soup.find("div", {"class": "fd-page-feed"}).find("script", {"type": "application/ld+json"}).contents[0]
+        l = json.loads(js)
+        directions = l["recipeInstructions"].replace('. ','.').split('.')
+        for step in directions:
+            if len(step)>1:
+                yield step
 
     def note(self):
-        return ""
+        js = self.soup.find("div", {"class": "fd-page-feed"}).find("script", {"type": "application/ld+json"}).contents[0]
+        l = json.loads(js)
+        description = l["description"]
+        return description
